c1b7a91a8bd2796c7fc20de612946aae43e54b00
Arthur1928 <165573881+Arthur1928@users.noreply.github.com>
2024-03-31 19:14:34 +0400

    Add files via upload

---
+++ index_nof.js
@@ -0,0 +1,12 @@
+// Begin XHTML adjustment
+$(document).ready(function(){
+	if (jQuery.browser.msie && jQuery.browser.version.substr(0, 2) == "6.") {
+		$(".nof-clearfix").each(function (i) {
+			$(this).append("<div style='clear:both'/>");
+			$(this).removeClass("nof-clearfix");
+		});
+	}
+});
+
+// End XHTML adjustment
+
